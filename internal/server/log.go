112ae6e26bba57150ba64c9c7b1ec767cbd61c94
yuya-morimoto <gyu2243dev@gmail.com>
2023-03-25 23:56:13 +0900

    create: first

---
+++ internal/server/log.go
@@ -0,0 +1,41 @@
+package server
+
+import (
+	"fmt"
+	"sync"
+)
+
+var ErrOffsetNotFound = fmt.Errorf("offset not found")
+
+type Record struct {
+	Value  []byte `json:"value"`
+	Offset uint64 `json:"offset"`
+}
+
+type Log struct {
+	mu      sync.Mutex
+	records []Record
+}
+
+func NewLog() *Log {
+	return &Log{}
+}
+
+// スライスにレコードを追加
+func (c *Log) Append(record Record) (uint64, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	record.Offset = uint64(len(c.records))
+	c.records = append(c.records, record)
+	return record.Offset, nil
+}
+
+// インデックスを使用してレコードを検索する
+func (c *Log) Read(offset uint64) (Record, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if offset >= uint64(len(c.records)) {
+		return Record{}, ErrOffsetNotFound
+	}
+	return c.records[offset], nil
+}
